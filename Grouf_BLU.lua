52368d5f7e36cb97dd6e5813faaf30024916306d
Grouf <grouf31@gmail.com>
2014-02-22 01:35:00 -0500

    BLM first draft

---
+++ Grouf_BLU.lua
@@ -0,0 +1,14 @@
+--[[
+		=========================
+		|   B L U E   M A G E   | 
+		=========================
+]]
+
+ 
+ --[[ Spells that have static effects, not modified by skill or stats
+    blue_no_skill_spell_list = S{'Cocoon','Harden Shell','Refueling','Zephyr Mantle','Regeneration','Animating Wail','Battery Charge',
+        ,'Winds of Promy.','Feather Barrier','Memento mori','Saline Coat','Warm-Up','Amplification','Exuviation','Fantod'}
+		
+]]
+
+-- if spell.type == 'BlueMagic' then
