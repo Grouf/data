f52707753a47fb33567045245d78a1789fea548e
Grouf <grouf31@gmail.com>
2014-02-22 20:55:47 -0500

    refine set, add JA/Relic rules

--- Grouf_GEO.lua
+++ Grouf_GEO.lua
@@ -24,10 +24,10 @@ function get_sets()
 		hands="Hagondes Cuffs",left_ring="Irrwisch Ring",right_ring="Omega Ring",back="Refraction Cape",
 		waist="Aswang Sash",legs="Orvail Pants +1",feet="Hagondes Sabots"}
 		
-	--sets.precast.FastCast = {main="Plenitas Virga",sub="Arbuda Grip",ammo="Impatiens",head="Nares Cap",
-	--	neck="Orunmila's Torque",left_ear="Loquac. Earring",body="Anhur Robe",
-	--	hands="Bokwus Gloves",back="Lifestream Cape",
-	--	waist="Witful Belt",legs="Geomancy Pants",feet="Augur's Gaiters"}
+	sets.precast.FastCast = {main="Lehbrailg +2",sub="Elder's Grip",head="Hagondes Hat",
+		left_ear="Loquac. Earring",body="Anhur Robe",
+		hands="Hagondes Cuffs",back="Swith Cape",
+		waist="Witful Belt",legs="Geomancy Pants",feet="Hagondes Sabots"}  --Geo. Pants = 11%
 	
 	sets.precast.Geomancy = {main="Dowser's Wand",sub="Genbu's Shield",range="Filiae Bell",head="Geomancy Galero",
 		neck="Morgana's Choker",left_ear="Loquac. Earring",body="Geomancy Tunic",
@@ -35,8 +35,8 @@ function get_sets()
 		waist="Witch Sash",legs="Geomancy Pants",feet="Geomancy Sandals"}
 		
 	--Enhancing needs work
-	sets.precast.Enhancing = {body="Anhur Robe",neck="Stone Gorget",hands="Ayao's Gages",
-		left_ear="Loquac. Earring",left_ring="Sangoma Ring",right_ring="Omega Ring",
+	sets.precast.Enhancing = {body="Anhur Robe",neck="Stone Gorget",hands="Augur's Gloves",
+		left_ear="Loquac. Earring",
 		waist="Siegel Sash",legs="Shedir Seraweels",feet="Geomancy Sandals"}
 		
 	sets.precast.ElementalMagic = {main="Lehbrailg +2",sub="Elder's Grip",ammo="Witchstone",head="Hagondes Hat",
@@ -74,19 +74,59 @@ function precast(spell)
 	elseif spell.skill == 'Geomancy' then
 		equip(sets.precast.Geomancy)
 	elseif spell.skill == 'EnhancingMagic' then
-		equip(sets.precast.Enhancing)
+		if spell.english == 'Stoneskin' or spell.english == 'Blink' or 
+		spell.english == 'Aquaveil' or spell.english == 'Phalanx' then
+			equip(sets.precast.Enhancing)
+		else
+			equip(sets.precast.FastCast)
+			send_command('@input /echo FastCast set on')
+		end
 	elseif spell.skill == 'ElementalMagic' then
 		equip(sets.precast.ElementalMagic)
+		if spell.element == 'Earth' then
+			equip({neck="Quanpur Necklace"})
+		end
 	elseif spell.skill == 'DarkMagic' then
 		equip(sets.precast.DarkMagic)
 	elseif spell.skill == 'EnfeeblingMagic' then
 		equip(sets.precast.Enfeeble)
 	elseif spell.skill == 'HealingMagic' then
 		equip(sets.precast.Cure)
-	elseif spell.type=="WeaponSkill" then
+	elseif spell.type=='WeaponSkill' then
 		equip(sets.TP.WS)
+	elseif spell.type=='JobAbility' then
+		--[[ R E L I C ]]--
+		if spell.english=="Collimated Fervor" then
+			equip({head="Bagua Galero"})
+			disable('head')
+			send_command('@input /echo Relic head on, head disable')
+		elseif spell.english=="Bolster" then
+			equip({body="Bagua Tunic"})
+			disable('body')
+			send_command('@input /echo Relic body on, body disable')
+		elseif spell.english=="Full Circle" then
+			equip({hands="Bagua Mitaines"})
+			disable('hands')
+			send_command('@input /echo Relic hands on, hands disable')
+		elseif spell.english=="Mending Halation" then
+			equip({legs="Bagua Pants"})
+			disable('legs')
+			send_command('@input /echo Relic legs on, legs disable')
+		elseif spell.english=="Radial Arcana" then
+			equip({feet="Bagua Sandals"})
+			disable('feet')
+			send_command('@input /echo Relic feet on, feet disable')
+		end
 	end
 end
+
+--[[Relic augments:
+Head: Bagua Galero: Primeval Zeal
+Body: Bagua Tunic: Bolster
+Hands: Bagua Mitaines: Curative Recantation
+Legs: Bagua Pants: Mending Halation
+Feet: Bagua Sandals: Radial Arcana
+]]
 
 function midcast(spell)
 end
@@ -109,5 +149,22 @@ function status_change(new,old)
 	end
 end
 
-function buff_change(status,gain_or_loss)
+function buff_change(status,gain_or_loss) --gain_or_loss = True if gained, False if lost
+	if status=='Collimated Fervor' and not gain_or_loss then
+		enable('head')
+		send_command('@input /echo Collimated Fervor off, head enabled')
+	elseif status=='Bolster' and not gain_or_loss then --if buff 'Bolster' and not false (not false = true) bolster lost
+		enable('body')
+		send_command('@input /echo Bolster off, body enabled')
+	elseif status=='Full Circle' and not gain_or_loss then
+		enable('hands')
+		send_command('@input /echo Full Circle off, hands enabled')
+	elseif status=='Mending Halation' and not gain_or_loss then
+		enable('legs')
+		send_command('@input /echo Full Circle off, hands enabled')
+	elseif status=='Radial Arcana' and not gain_or_loss then
+		enable('feet')
+		send_command('@input /echo Full Circle off, hands enabled')
+	end
+		
 end
