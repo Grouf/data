eefb40167ef0da3128a080fab5a9c5abc25b8d1f
Grouf <grouf31@gmail.com>
2014-03-21 17:29:00 -0400

    Updated and Tested
    Migawari still needs work

--- Grouf_NIN.lua
+++ Grouf_NIN.lua
@@ -5,15 +5,15 @@
 ]]
 
 function get_sets()
-	NinjitsuNuke = {
-		['Katon: Ichi'],['Suiton: Ichi'],['Doton: Ichi'],['Hyoton: Ichi'],['Huton: Ichi'],['Raiton: Ichi'],
-		['Katon: Ni'],['Suiton: Ni'],['Doton: Ni'],['Hyoton: Ni'],['Huton: Ni'],['Raiton: Ni'],
-		['Katon: San'],['Suiton: San'],['Doton: San'],['Hyoton: San'],['Huton: San'],['Raiton: San']
+	NinjutsuNuke = T{
+		'Katon: Ichi','Suiton: Ichi','Doton: Ichi','Hyoton: Ichi','Huton: Ichi','Raiton: Ichi',
+		'Katon: Ni','Suiton: Ni','Doton: Ni','Hyoton: Ni','Huton: Ni','Raiton: Ni',
+		'Katon: San','Suiton: San','Doton: San','Hyoton: San','Huton: San','Raiton: San'
 		}
 		
-	NinjitsuEnfeeble = {
-		['Hojo: Ichi'],['Kurayami: Ichi'],['Dokumori: Ichi'],['Jubaku: Ichi'],['Yurin: Ichi'],
-		['Hojo: Ni'],['Kurayami: Ni'],['Dokumori: Ni'],['Jubaku: Ni'],['Yurin: Ni']
+	NinjutsuEnfeeble = T{
+		'Hojo: Ichi','Kurayami: Ichi','Dokumori: Ichi','Jubaku: Ichi','Yurin: Ichi',
+		'Hojo: Ni','Kurayami: Ni','Dokumori: Ni','Jubaku: Ni','Yurin: Ni'
 		}
 		
 	sets.precast = {}
@@ -22,14 +22,14 @@ function get_sets()
 	sets.precast.Yonin = {legs="Iga Hakama +2"}
 	sets.precast.Futae = {hands="Iga Tekko +2"}
 
-	sets.precast.Ranged= {head="Uk'uxkaj Cap",neck="Arctier's Torque",left_ear="Clearview earring",
+	sets.precast.Ranged= {head="Uk'uxkaj Cap",neck="Huani Collar",left_ear="Clearview earring",
 		right_ear="Bladeborn Earring",body="Iuitl Vest",hands="Hachiya Tekko",left_ring="Paqichikaji Ring",
 		right_ring="Jalzahn's Ring",back="Yokaze Mantle",waist="Buccaneer's Belt",
-		legs="Thur. Tights +1",feet="Pillager's Poulaines"} --need to verify
+		legs="Thur. Tights +1",feet="Pillager's Poulaines"}
 	
-	sets.precast.WS= {ammo="Qirmiz Tathlum",head="Uk'uxkaj Cap",neck="Tlamiztli Collar",left_ear="Steelflash Earring",
+	sets.precast.WS= {head="Uk'uxkaj Cap",neck="Tlamiztli Collar",left_ear="Steelflash Earring",
 		right_ear="Bladeborn Earring",body="Manibozho Jerkin",hands="Otronif Gloves",left_ring="Spiral Ring",
-		right_ring="Rajas Ring",back="Buquwik Cape",waist="Prosilio Belt",
+		right_ring="Rajas Ring",back="Vassal's Mantle",waist="Prosilio Belt",
 		legs="Manibozho Brais",feet="Otronif Boots"}
 	
 	sets.precast["Blade Hi"] = set_combine(sets.precast.WS,{neck="Shadow Gorget",left_ring="Solemn Ring",
@@ -37,48 +37,48 @@ function get_sets()
 	
 	sets.precast["Blade Shun"] = sets.precast.WS --DEX
 	
-	sets.precast["Blade Kamu"] = set_combine(sets.precast.WS,{right_ring="Flame Ring"})<!--STR50 INT50-->
+	sets.precast["Blade Kamu"] = sets.precast.WS --STR50 INT50
 	
 	sets.precast["Blade Jin"] = sets.precast.WS --STR30 DEX30
 	
-	sets.precast["Blade Yu"] = set_combine(sets.precast.WS,{neck="Breeze Gorget",left_ear="Friomisi Earring",
-		right_ear="Novio Earring",back="Toro Cape",waist="Breeze Belt",} --DEX50 INT50
+	sets.precast["Blade Yu"] = set_combine(sets.precast.WS,{left_ear="Friomisi Earring",
+		right_ear="Hecate's Earring",back="Toro Cape"}) --DEX50 INT50
 	
 	sets.precast.Utsusemi= {head="Athos's Chapeau",neck="Magoraga Beads",left_ear="Phawaylla Earring",
-		right_ear="Loquac. Earring",body="Otronif Harness",hands="Kog. Tekko +2",left_ring="",
+		right_ear="Loquac. Earring",body="Otronif Harness",hands="Kog. Tekko +2",left_ring="Prolix Ring",
 		right_ring="",back="Yokaze Mantle",waist="Phasmida Belt",
-		legs="Otronif Brais",feet="Iga Kyahan +2"}
+		legs="Kaabnax Trousers",feet="Iga Kyahan +2"} --30% Haste
 	
 	sets.precast["Migawari: Ichi"] = set_combine(sets.precast.Utsusemi,{body="Iga Ningi +2"})
 	
 	sets.precast.Enfeeble= {head="Hachiya Hatsuburi",neck="Stoicheion Medal",left_ear="Lifestorm Earring",
-		right_ear="Psystorm Earring",body="Otronif Harness",hands="Kog. Tekko +2",left_ring="",
+		right_ear="Psystorm Earring",body="Otronif Harness",hands="Kog. Tekko +2",left_ring="Prolix Ring",
 		right_ring="Omega Ring",back="Yokaze Mantle",waist="Phasmida Belt",
-		legs="Otronif Brais",feet="Hachiya Kayahan"}  --needs verify
+		legs="Kaabnax Trousers",feet="Hachiya Kyahan"}  --32% Haste, 224 Magic Accuracy
 	
 	sets.precast.Nuke= {head="Koga Hatsuburi",neck="Stoicheion Medal",left_ear="Friomisi Earring",
-		right_ear="Hecate's Earring",body="Otronif Harness",hands="Kog. Tekko +2",left_ring="Icesoul Ring",
-		right_ring="",back="Toro Cape",waist="Koga Sarashi",
-		legs="Shneddick Tights",feet="Hachiya Kayahan"}
+		right_ear="Hecate's Earring",body="Otronif Harness",hands="Kog. Tekko +2",left_ring="Prolix Ring",
+		right_ring="Icesoul Ring",back="Toro Cape",
+		legs="Quiahuiz Trousers",feet="Hachiya Kayahan"}
 	
 	sets.precast.Step = {head="Ejekamal Mask",neck="Asperity Necklace",left_ear="Steelflash Earring",
-		right_ear="Heartseeker Earring",body="Manibozho Jerkin",hands="",left_ring="Rajas Ring",
-		right_ring="Epona's Ring",back="",waist="Phasmida Belt",
-		legs="Manibozho Brais",feet=""}--Needs Verify
+		right_ear="Heartseeker Earring",body="Manibozho Jerkin",hands="Buremte Gloves",left_ring="Rajas Ring",
+		right_ring="Epona's Ring",back="Yokaze Mantle",waist="Phasmida Belt",
+		legs="Hachiya Hakama",feet="Otronif Boots"} --Primary Acc: 907
 	
 		
 	sets.TP = {}
-	sets.TP.DD = {ammo="Qirmiz Tathlum",head="Otronif Mask",neck="Asperity Necklace",left_ear="Steelflash Earring",
+	sets.TP.DD = {head="Uk'Uxkaj Cap",neck="Asperity Necklace",left_ear="Steelflash Earring",
 		right_ear="Bladeborn Earring",body="Thaumas Coat",hands="Otronif Gloves",left_ring="Epona's Ring",
 		right_ring="Rajas Ring",back="Yokaze Mantle",waist="Windbuffet Belt",
-		legs="Quiahuiz Leggings",feet="Otronif Boots"}
+		legs="Quiahuiz Trousers",feet="Otronif Boots"}
 		
-	sets.TP.EVA = set_combine(sets.TP.DD,{head="Uk'uxkaj Cap",neck="Torero Torque",left_ear="Ethereal Earring",
+	sets.TP.EVA = set_combine(sets.TP.DD,{neck="Torero Torque",left_ear="Ethereal Earring",
 		right_ear="Musical Earring",body="Otronif Harness",
 		right_ring="Defending Ring",waist="Koga sarashi",
-		legs="Otronif Brais"}  --Needs Verify
+		legs="Otronif Brais"})  --Needs Verify
 	
-	sets.TP.DT= {ammo="Demonry Stone",head="Uk'uxkaj Cap",neck="Twilight Torque",left_ear="Dudgeon Earring",
+	sets.TP.DT= {head="Uk'uxkaj Cap",neck="Twilight Torque",left_ear="Dudgeon Earring",
 		right_ear="Heartseeker earring",body="Otronif Harness",hands="Otronif Gloves",left_ring="Dark Ring",
 		right_ring="Dark Ring",back="Ik Cape",waist="Flume Belt",
 		legs="Kaabnax Trousers",feet="Otronif Boots"}
@@ -86,20 +86,26 @@ function get_sets()
 	sets.aftercast = {}
 	sets.aftercast.TP = sets.TP.DD
 	sets.aftercast.Idle = set_combine(sets.aftercast.TP,{feet="Danzo Sune-Ate"})
-	send_command('input /macro book 6;wait .1;input /macro set 10')
+	send_command('input /macro book 13;wait .1;input /macro set 1')
 	
 end
 
 function precast(spell)
+	--windower.add_to_chat(204, 'Casting: ' ..spell.english.. ' ' ..spell.type)
 	if sets.precast[spell.english] then
 		equip(sets.precast[spell.english])
-	elseif spell.type == "Ninjitsu" then
-		if NinjitsuNuke[spell.english] then
+		--windower.add_to_chat(204, 'Spell name as set: ' ..spell.english)
+	elseif spell.type == "Ninjutsu" then
+		--windower.add_to_chat(204, 'Ninjitsu Detected: ' ..spell.english.. ' ' ..spell.type)
+		if NinjutsuNuke:contains(spell.english) then
 			equip(sets.precast.Nuke)
-		elseif NinjitsuEnfeeble[spell.english] then
+			--windower.add_to_chat(204, 'Nuke Detected')
+		elseif NinjutsuEnfeeble:contains(spell.english) then
 			equip(sets.precast.Enfeeble)
+			--windower.add_to_chat(204, 'Enfeeble Detected')
 		else
 			equip(sets.precast.Utsusemi)
+			--windower.add_to_chat(204, 'Other Detected, Utsusemi Set: ' ..spell.english)
 		end
 		
 	elseif spell.type=="WeaponSkill" then
