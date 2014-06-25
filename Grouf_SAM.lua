761fbbc38f817d72e6e05fdbc6901bf8d1fc7462
Grouf <grouf31@gmail.com>
2014-06-25 11:12:23 -0400

    Gear update, process update

--- Grouf_SAM.lua
+++ Grouf_SAM.lua
@@ -15,7 +15,7 @@ function get_sets()
 	
 	-- Weapon Skill sets:  Damage --
 	sets.WS.DD = {head="Yaoyotl Helm", neck="Tlamiztli Collar", left_ear="Steelflash Earring", 
-		right_ear="Bladeborn Earring", body="Phorcys Korazin", hands="Mikinaak Gauntlets", left_ring="Spiral Ring", 
+		right_ear="Bladeborn Earring", body="Miki. Breastplate", hands="Mikinaak Gauntlets", left_ring="Cho'j Band", 
 		right_ring="Rajas Ring", back="Buquwik Cape", waist="Prosilio Belt", 
 		legs="Miki. Cuisses", feet="Mikinaak Greaves" }
 		
@@ -30,8 +30,8 @@ function get_sets()
 	
 	-- Weapon Skill sets:  Accuracy --
 	sets.WS.Acc = {head="Yaoyotl Helm", neck="Tlamiztli Collar", left_ear="Steelflash Earring", 
-		right_ear="Bladeborn Earring", body="Miki. Breastplate", hands="Mikinaak Gauntlets", left_ring="Spiral Ring", 
-		right_ring="Rajas Ring", back="Takaha Mantle", waist="Prosilio Belt", 
+		right_ear="Bladeborn Earring", body="Miki. Breastplate", hands="Mikinaak Gauntlets", left_ring="Cho'j Band", 
+		right_ring="Rajas Ring", back="Takaha Mantle", waist="Caudata Belt", 
 		legs="Miki. Cuisses", feet="Mikinaak Greaves" }
 		
 	sets.WS.Acc["Tachi: Fudo"] = set_combine(sets.WS.Acc,{neck="Light Gorget"}) --STR60
@@ -43,20 +43,19 @@ function get_sets()
 	
 	sets.WS.Acc["Tachi: Shoha"] = set_combine(sets.WS.Acc,{neck="Shadow Gorget"})
 	
-	
+	sets.JA = {}
+	sets.JA.Meditate = {head="Wakido Kabuto", hands="Sakonji Kote"}
+	sets.JA.Hasso = {hands="Wakido Kote", legs="Unkai Haidate +2"}
+	sets.JA["Warding Circle"] = {head="Wakido Kabuto"}
+	sets.JA.Sekkanoki = {hands="Unkai Kote +2"}
+	sets.JA["Blade Bash"] = {hands="Sakonji Kote"}
 	
 	sets.precast = {}
 	
-	sets.precast.Meditate = {head="Wakido Kabuto", hands="Sakonji Kote"}
-	sets.precast.Hasso = {hands="Wakido Kote", legs="Unkai Haidate +2"}
-	sets.precast["Warding Circle"] = {head="Wakido Kabuto"}
-	sets.precast.Sekkanoki = {hands="Unkai Kote +2"}
-	sets.precast["Blade Bash"] = {hands="Sakonji Kote"}
-	
 	sets.precast.Ranged = {head="Yaoyotl Helm",neck="Ocachi Gorget",left_ear="Clearview earring",
 		right_ear="Bladeborn Earring",body="Otronif Harness",hands="Unkai Kote +2",left_ring="Paqichikaji Ring",
-		right_ring="Jalzahn's Ring",back="Buquwik Cape",waist="Buccaneer's Belt",
-		legs="Wakido Haidate",feet="Wakido Sune-Ate"} -- R.Acc=647  R.Att=744
+		right_ring="Fistmele Ring",back="Buquwik Cape",waist="Buccaneer's Belt",
+		legs="Wakido Haidate",feet="Wakido Sune-Ate"} -- R.Acc=693  R.Att=797 iLvl=114
 		
 	sets.precast["Apex Arrow"] = sets.precast.Ranged
 		--[[neck="Light Gorget", 
@@ -73,35 +72,37 @@ function get_sets()
 	
 	sets.precast["Tachi: Shoha"] = sets.WS.DD["Tachi: Shoha"]
 	
-	sets.precast.Step = {head="Yaoyotl Helm",neck="Unkai Nodowa",left_ear="Steelflash Earring",
-		right_ear="Heartseeker Earring",body="Miki. Breastplate",hands="Buremte Gloves",left_ring="Rajas Ring",
-		right_ring="Spiral Ring",back="Takaha Mantle",waist="Phasmida Belt",
-		legs="Miki. Cuisses",feet="Wakido Sune-Ate"} --Primary Acc: 834
+	sets.precast.Step = {head="Yaoyotl Helm", neck="Unkai Nodowa", left_ear="Steelflash Earring",
+		right_ear="Heartseeker Earring", body="Miki. Breastplate", hands="Buremte Gloves", left_ring="Spiral Ring",
+		right_ring="Rajas Ring", back="Takaha Mantle", waist="Phasmida Belt",
+		legs="Miki. Cuisses", feet="Wakido Sune-Ate"} --Primary Acc: 834
 	
 		
 	sets.TP = {}
-	sets.TP.DD = {head="Yaoyotl Helm", neck="Asperity Necklace", left_ear="Steelflash Earring", 
-		right_ear="Bladeborn Earring", body="Unkai Domaru +2", hands="Otronif Gloves", left_ring="Spiral Ring", 
+	sets.TP.DD = {sub="Pole Grip", head="Otomi Helm", neck="Asperity Necklace", left_ear="Steelflash Earring", 
+		right_ear="Bladeborn Earring", body="Wakido Domaru", hands="Otronif Gloves", left_ring="Cho'j Band", 
 		right_ring="Rajas Ring", back="Takaha mantle", waist="Cetl Belt", 
-		legs="Unkai Haidate +2", feet="Otronif Boots" }  	--??? With pole Grip Double Attack = 20% ???
-															--??? Store TP = 51 ???
-															--Avg Item Level = 111 ?
-															--Acc = 766
-															--Att = 855
-															--Eva = 564
-															--Def = 593
+		legs="Wakido Haidate", feet="Otronif Boots" }  	--With pole Grip Double Attack = 20%
+															--Store TP = 55
+															--Haste = 
+															--(With IONIS)
+															--Avg Item Level = 115
+															--Acc = 830
+															--Att = 960
+															--Eva = 631
+															--Def = 869
 	
-	sets.TP.Acc = {head="Yaoyotl Helm", neck="Asperity Necklace", left_ear="Steelflash Earring", 
-		right_ear="Bladeborn Earring", body="Unkai Domaru +2", hands="Otronif Gloves", left_ring="Spiral Ring", 
+	sets.TP.Acc = {sub="Tzacab Grip", head="Yaoyotl Helm", neck="Asperity Necklace", left_ear="Steelflash Earring", 
+		right_ear="Bladeborn Earring", body="Miki. Breastplate", hands="Otronif Gloves", left_ring="Cho'j Band", 
 		right_ring="Rajas Ring", back="Takaha mantle", waist="Cetl Belt", 
-		legs="Unkai Haidate +2", feet="Otronif Boots" } --needs more work, added quick for Dimi
+		legs="Miki. Cuisses", feet="Wakido Sune-Ate" } --needs more work, added quick for Dimi
 		
 	--[[sets.TP.EVA = set_combine(sets.TP.DD,{neck="Torero Torque",left_ear="Ethereal Earring",
 		right_ear="Musical Earring",body="Otronif Harness",
 		right_ring="Defending Ring",waist="Koga sarashi",
 		legs="Otronif Brais"})  --Needs Verify]]
 	
-	sets.TP.DT= {head="Yaoyotl Helm", neck="Twilight Torque", left_ear="Steelflash Earring", 
+	sets.TP.DT= {sub="Pole Grip", head="Yaoyotl Helm", neck="Twilight Torque", left_ear="Steelflash Earring", 
 		right_ear="Bladeborn Earring", body="Otronif Harness", hands="Otronif Gloves", left_ring="Dark Ring", 
 		right_ring="Dark Ring", back="Ik Cape", waist="Flume Belt", 
 		legs="Otronif Brais", feet="Otronif Boots" }
@@ -109,21 +110,42 @@ function get_sets()
 	sets.aftercast = {}
 	sets.aftercast.TP = sets.TP.DD
 	sets.aftercast.Idle = set_combine(sets.aftercast.TP,{feet="Danzo Sune-Ate"})
-	send_command('input /macro book 12;wait .1;input /macro set 10')
+	--send_command('input /macro book 12;wait .1;input /macro set 10')
+	set_macro_book()
 	
 end
 
 function precast(spell)
-	--windower.add_to_chat(204, 'Casting: ' ..spell.english.. ' ' ..spell.type)
-	if sets.precast[spell.english] then
+	--windower.add_to_chat(14, 'Precast: spell type= ' ..spell.type.. ' .english=' ..spell.english)
+	--[[if spell.prefix ~= '/jobability' and spell.type ~= 'WeaponSkill' then
+		windower.add_to_chat(14, 'not JobAbility or WeaponSkill so Fast Cast')
+		equip(sets.precast.FastCast)
+	]]
+	if spell.type == "WeaponSkill" then
+		--windower.add_to_chat(14, 'WeaponSkill found: ' ..spell.english)
+		if sets.precast[spell.english] then
+			equip(sets.precast[spell.english])
+			--windower.add_to_chat(14, 'Named WS set: ' ..spell.english)
+		else
+			equip(sets.precast.WS)
+			--windower.add_to_chat(14, 'General WS set: ' ..spell.english)
+		end
+	
+	elseif spell.prefix == "/jobability" then
+		--windower.add_to_chat(14, '/jobability found: ' ..spell.english)
+		if sets.JA[spell.english] then
+			equip(sets.JA[spell.english])
+		elseif spell.type == 'Step' or spell.english == 'Violent Flourish' then
+			equip(sets.precast.Step)
+		end
+	
+	elseif sets.precast[spell.english] then
+		--windower.add_to_chat(14, 'Specific precast: ' ..spell.english)
 		equip(sets.precast[spell.english])
-		--windower.add_to_chat(204, 'Spell name as set: ' ..spell.english)
-	elseif spell.type=="Weapon Skill" then
-		equip(sets.precast.WS)
-	elseif string.find(spell.english,'Step') or 
-		string.find(spell.english,'Violent Flourish') then
-		equip(sets.precast.Step)
+	
+	
 	end
+	
 end
 
 function midcast(spell)
@@ -196,5 +218,18 @@ function self_command(command)
 		equip(sets.aftercast.TP)
 	else
 		equip(sets.aftercast.Idle)
+	end
+end
+
+function set_macro_book()
+	-- Default macro book & page
+	if player.sub_job == 'WAR' then
+		send_command('input /macro book 12;wait .1;input /macro set 10')
+	elseif player.sub_job == 'DNC' then
+		send_command('input /macro book 12;wait .1;input /macro set 9')
+	elseif player.sub_job == 'NIN' then
+		send_command('input /macro book 12;wait .1;input /macro set 8')
+	else
+		send_command('input /macro book 12;wait .1;input /macro set 10')
 	end
 end
