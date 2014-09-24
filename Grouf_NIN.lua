a5eacfea6402c8bfb63017ea58be1f58312b4d0c
Grouf <grouf31@gmail.com>
2014-09-24 14:13:14 -0400

    Major rework

--- Grouf_NIN.lua
+++ Grouf_NIN.lua
@@ -16,59 +16,69 @@ function get_sets()
 		'Hojo: Ni','Kurayami: Ni','Dokumori: Ni','Jubaku: Ni','Yurin: Ni'
 		}
 		
-	sets.precast = {}
 	
-	sets.precast.Innin = {head="Iga Zukin +2"}
-	sets.precast.Yonin = {legs="Iga Hakama +2"}
-	sets.precast.Futae = {hands="Iga Tekko +2"}
-	sets.precast['Mijin Gakure'] = {legs="Mochizuki Hakama"}
-
-	sets.precast.Ranged = {head="Uk'uxkaj Cap",neck="Ocachi Gorget",left_ear="Clearview earring",
-		right_ear="Bladeborn Earring",body="Mochi. Chainmail",hands="Hachiya Tekko",left_ring="Fistmele Ring",
-		right_ring="Paqichikaji Ring",back="Yokaze Mantle",waist="Buccaneer's Belt",
-		legs="Thur. Tights +1",feet="Scopuli Nails +1"}
+	sets.JA = {}
+	sets.JA.Innin = {head="Iga Zukin +2"}
+	sets.JA.Yonin = {legs="Iga Hakama +2"}
+	sets.JA.Futae = {hands="Iga Tekko +2"}
+	sets.JA.Sange = {body="Mochi. Chainmail"}
+	sets.JA['Mijin Gakure'] = {legs="Mochizuki Hakama"}
 	
-	sets.precast.WS = {head="Whirlpool Mask",neck="Tlamiztli Collar",left_ear="Moonshade Earring",
-		right_ear="Bladeborn Earring",body="Manibozho Jerkin",hands="Qaaxo Mitaines",left_ring="Cho'j Band",
-		right_ring="Rajas Ring",back="Vespid Mantle",waist="Prosilio Belt",
-		legs="Manibozho Brais",feet="Otronif Boots +1"}
-	
-	sets.precast["Blade Hi"] = set_combine(sets.precast.WS,{neck="Shadow Gorget",hands="Iga Tekko +2"}) --AGI
-	
-	sets.precast["Blade Shun"] = sets.precast.WS --DEX
-	
-	sets.precast["Blade Kamu"] = sets.precast.WS --STR50 INT50
-	
-	sets.precast["Blade Jin"] = sets.precast.WS --STR30 DEX30
-	
-	sets.precast["Blade Yu"] = set_combine(sets.precast.WS,{left_ear="Friomisi Earring",
-		right_ear="Hecate's Earring",back="Toro Cape"}) --DEX50 INT50
-	
-	--sets.precast.FastCast = NEEDS WORK
-	
-	sets.precast.Utsusemi = {head="Athos's Chapeau",neck="Magoraga Beads",left_ear="Phawaylla Earring",
-		right_ear="Loquac. Earring",body="Mochi. Chainmail",hands="Mochizuki Tekko",left_ring="Prolix Ring",
-		back="Yokaze Mantle",waist="Phasmida Belt",
-		legs="Kaabnax Trousers",feet="Iga Kyahan +2"} --30% Haste
-	
-	sets.precast["Migawari: Ichi"] = set_combine(sets.precast.Utsusemi,{body="Iga Ningi +2"})
-	
-	sets.precast.Enfeeble = {head="Hachiya Hatsuburi",neck="Stoicheion Medal",left_ear="Lifestorm Earring",
-		right_ear="Psystorm Earring",body="Otronif Harness",hands="Mochizuki Tekko",left_ring="Prolix Ring",
-		right_ring="Sangoma Ring",back="Yokaze Mantle",waist="Phasmida Belt",
-		legs="Kaabnax Trousers",feet="Hachiya Kyahan"}  --32% Haste, 224 Magic Accuracy
-	
-	sets.precast.Nuke = {--[[head="Koga Hatsuburi",]]neck="Stoicheion Medal",left_ear="Friomisi Earring",
-		right_ear="Hecate's Earring",body="Otronif Harness",hands="Mochizuki Tekko",left_ring="Prolix Ring",
-		right_ring="Acumen Ring",back="Toro Cape",
-		legs="Quiahuiz Trousers",feet="Hachiya Kyahan"}
-	
-	sets.precast.Step = {head="Whirlpool Mask",neck="Asperity Necklace",left_ear="Steelflash Earring",
+	sets.JA.Step = {head="Whirlpool Mask",neck="Asperity Necklace",left_ear="Steelflash Earring",
 		right_ear="Heartseeker Earring",body="Manibozho Jerkin",hands="Buremte Gloves",left_ring="Rajas Ring",
 		right_ring="Beeline Ring",back="Yokaze Mantle",waist="Phasmida Belt",
 		legs="Hachiya Hakama",feet="Otronif Boots +1"} --Primary Acc: 913
 	
-		
+	sets.JA['Violent Flourish'] = set_combine(sets.JA.Step, {neck="Stoicheion Medal", 
+		left_ear="Psystorm Earring", right_ear="Lifestorm Earring", left_ring="Perception Ring", 
+		right_ring="Sangoma Ring"})
+	
+	sets.precast = {}
+	
+	sets.precast.Ranged = {head="Uk'uxkaj Cap",neck="Ocachi Gorget", left_ear="Clearview earring",
+		right_ear="Bladeborn Earring", body="Mochi. Chainmail", hands="Hachiya Tekko", left_ring="Fistmele Ring",
+		right_ring="Paqichikaji Ring", back="Yokaze Mantle", waist="Buccaneer's Belt",
+		legs="Thur. Tights +1", feet="Scopuli Nails +1"}
+	
+	
+	sets.precast.FastCast = {head="Athos's Chapeau",
+		right_ear="Loquac. Earring", body="Mochi. Chainmail",hands="Mochizuki Tekko", left_ring="Prolix Ring",
+		back="Yokaze Mantle", waist="Phasmida Belt", legs="Kaabnax Trousers"} --30% Haste
+	
+	sets.midcast = {}
+	sets.midcast.Utsusemi = {neck="Magoraga Beads", feet="Iga Kyahan +2"}
+	
+	sets.midcast["Migawari: Ichi"] = {body="Iga Ningi +2"}
+	
+	sets.midcast.Enfeeble = {head="Hachiya Hatsuburi",neck="Stoicheion Medal",left_ear="Lifestorm Earring",
+		right_ear="Psystorm Earring",body="Qaaxo Harness",
+		left_ring="Perception Ring", right_ring="Sangoma Ring",back="Yokaze Mantle",
+		feet="Hachiya Kyahan"}  --32% Haste, 230 Magic Accuracy
+	
+	sets.midcast.Nuke = {head="Mochi. Hatsuburi", neck="Stoicheion Medal",left_ear="Friomisi Earring",
+		right_ear="Crematio Earring",body="Qaaxo Harness",left_ring="Prolix Ring",
+		left_ring="Perception Ring", right_ring="Acumen Ring",back="Toro Cape",
+		legs="Quiahuiz Trousers",feet="Hachiya Kyahan"} --feet should be 109relic
+	
+	
+	sets.WS = {}
+	sets.WS.Base = {head="Whirlpool Mask",neck="Tlamiztli Collar",left_ear="Moonshade Earring",
+		right_ear="Bladeborn Earring",body="Manibozho Jerkin",hands="Qaaxo Mitaines",left_ring="Cho'j Band",
+		right_ring="Rajas Ring",back="Vespid Mantle",waist="Prosilio Belt +1",
+		legs="Manibozho Brais",feet="Otronif Boots +1"}
+	
+	sets.WS["Blade Hi"] = set_combine(sets.WS.Base,{neck="Shadow Gorget",hands="Iga Tekko +2"}) --AGI
+	
+	sets.WS["Blade Shun"] = sets.WS.Base --DEX
+	
+	sets.WS["Blade Kamu"] = sets.WS.Base --STR50 INT50
+	
+	sets.WS["Blade Jin"] = sets.WS.Base --STR30 DEX30
+	
+	sets.WS["Blade Yu"] = set_combine(sets.WS.Base,{left_ear="Friomisi Earring",
+		right_ear="Hecate's Earring",back="Toro Cape"}) --DEX50 INT50
+	
+	
 	sets.TP = {}
 	sets.TP.DD = {head="Whirlpool Mask",neck="Asperity Necklace",left_ear="Steelflash Earring",
 		right_ear="Bladeborn Earring",body="Qaaxo Harness",hands="Qaaxo Mitaines",left_ring="Epona's Ring",
@@ -93,34 +103,55 @@ function get_sets()
 end
 
 function precast(spell)
-	--windower.add_to_chat(204, 'Casting: ' ..spell.english.. ' ' ..spell.type)
-	if sets.precast[spell.english] then
-		equip(sets.precast[spell.english])
-		--windower.add_to_chat(204, 'Spell name as set: ' ..spell.english)
-	elseif spell.type == "Ninjutsu" then
-		--windower.add_to_chat(204, 'Ninjitsu Detected: ' ..spell.english.. ' ' ..spell.type)
-		if NinjutsuNuke:contains(spell.english) then
-			equip(sets.precast.Nuke)
-			--windower.add_to_chat(204, 'Nuke Detected')
-		elseif NinjutsuEnfeeble:contains(spell.english) then
-			equip(sets.precast.Enfeeble)
-			--windower.add_to_chat(204, 'Enfeeble Detected')
+	--windower.add_to_chat(14, 'Precast: spell=' ..spell.english.. ' spell type=' ..spell.type.. ' Skill=' ..spell.skill)
+	if spell.prefix ~= '/jobability' and spell.type ~= 'WeaponSkill' then
+		--windower.add_to_chat(14, 'not JobAbility or WeaponSkill so Fast Cast')
+		equip(sets.precast.FastCast)
+	
+	elseif spell.type == "WeaponSkill" then
+		if sets.WS[spell.english] then
+			equip(sets.WS[spell.english])
 		else
-			equip(sets.precast.Utsusemi)
-			--windower.add_to_chat(204, 'Other Detected, Utsusemi Set: ' ..spell.english)
+			equip(sets.WS.Base)
 		end
-		
-	elseif spell.type=="WeaponSkill" then
-		equip(sets.precast.WS)
 		if buffactive['Reive Mark'] then
 			equip({neck="Ygnas's Resolve +1"})
 		end
-	elseif spell.type == 'Step' or spell.english == 'Violent Flourish') then
-		equip(sets.precast.Step)
+	
+	elseif spell.prefix == "/jobability" then
+		if sets.JA[spell.english] then
+			equip(sets.JA[spell.english])
+		elseif spell.type == 'Step' then
+			equip(sets.JA.Step)
+		end
 	end
+	
 end
 
 function midcast(spell)
+	if spell.prefix == '/jobability' or spell.type == 'WeaponSkill' then
+	--midcast doesn't exist for JA or WS so cancel the processing of this function
+		--windower.add_to_chat(14, 'JobAbility or WeaponSkill; Midcast cancelled')
+		return
+	end
+	
+	if sets.midcast[spell.english] then
+		equip(sets.midcast[spell.english])
+		--windower.add_to_chat(9, 'Spell name as set: ' ..spell.english)
+	elseif spell.type == "Ninjutsu" then
+		--windower.add_to_chat(9, 'Ninjitsu Detected: ' ..spell.english.. ' ' ..spell.type)
+		if NinjutsuNuke:contains(spell.english) then
+			equip(sets.midcast.Nuke)
+			--windower.add_to_chat(9, 'Nuke Detected')
+		elseif NinjutsuEnfeeble:contains(spell.english) then
+			equip(sets.midcast.Enfeeble)
+			--windower.add_to_chat(9, 'Enfeeble Detected')
+		else
+			equip(sets.midcast.Utsusemi)
+			--windower.add_to_chat(9, 'Other Detected, Utsusemi Set: ' ..spell.english)
+		end
+	end
+	
 end
 
 function aftercast(spell)
@@ -149,11 +180,12 @@ function buff_change(buff_name,gain) --gain = True if gained, False if lost
 	if buff_name=='Migawari' then
 		if gain then
 			equip({body="Iga Ningi +2"}) -- Need to verify this works
-			send_command('@wait 0.5; gs disable body;')
-			windower.add_to_chat(14, 'Migawari status detected, Empy body on, Body disabled')
+			disable('body')
+			--send_command('@wait 0.5; gs disable body;')
+			windower.add_to_chat(9, 'Migawari status detected, Empy body on, Body disabled')
 		else
-			enable("body")
-			windower.add_to_chat(14, 'Migawari status lost, Body enabled')
+			enable('body')
+			windower.add_to_chat(9, 'Migawari status lost, Body enabled')
 		end
 	end
 	
